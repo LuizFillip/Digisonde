e99784b5951af02361c0ef68835c18f2cca8737a
LuizFillip <luizfillip@outlook.com>
2022-07-28 16:04:02 -0300

    Changing the function name and some kwords

--- plotVerticaldrift.py
+++ plotVerticaldrift.py
@@ -11,10 +11,58 @@ from prereversalEnhancement import *
 import astral 
 import datetime
 from astral.sun import sun
+import matplotlib.dates as dates
 
+def secundary_axis(ax, drift):
 
+    ax2 = ax.twiny()
+    data = np.empty(len(drift))
+    data[:] = np.nan
+    index = pd.to_datetime(drift.index + pd.Timedelta(hours = -3))
+    
+    xs = pd.Series(data= data,
+                   index = index)
+    
+    xs.plot(ax = ax2)
+    
+    ax2.spines['top'].set_visible(False)   
+    ax2.axes.yaxis.set_visible(False)
+    
+    ax2.xaxis.set_major_formatter(dates.DateFormatter('%H:%M'))
+    ax2.xaxis.set_major_locator(dates.HourLocator(interval = 1))
+ 
+   
+    ax2.xaxis.set_ticks_position('bottom') 
+    ax2.xaxis.set_label_position('bottom') 
+    ax2.spines['bottom'].set_position(('outward', 45))
+    
+    
+    
+    ax2.set(xlabel = "Time (LT)")
 
+def doy_str_format(date):
+    
+    doy = date.timetuple().tm_yday
+    
+    if doy < 10:
+        FigureName = f"00{doy}"
+        
+    elif doy > 10 and doy < 100:
+        FigureName = f"0{doy}"
+
+    else:
+        FigureName = f"{doy}"
+        
+    return FigureName
+  
+    
+  
 def plot(iono, day, 
+         twilightAngle = 18, 
+         latitude = -3.9,
+         longitude = -38.58,
+         fontsize = 14,
+         site = "Fortaleza",
          save = True):
     
     if save: 
@@ -50,114 +98,87 @@ def plot(iono, day,
               ylabel = "Velocity (m/s)", 
               ylim = [-90, 90])
     
-    ax[1].legend(drift.columns, loc = 'lower left',
-                 title = "Frequencies (MHz)", ncol = 6)
+    ax[1].legend(drift.columns, loc = 'lower left', 
+                 prop={'size': fontsize - 2},
+                 title = "Frequencies (MHz)", ncol = 3)
     
-    # =============================================================================
-    # TERMINATOR SOLAR AT 300 KM 
-    # =============================================================================
-    year, month, day = date.year, date.month, date.day
-    
-    observer = astral.Observer(latitude = -3.9, longitude = -38.58)
-    date = datetime.date(year, month, day)
-    infos = sun(observer, date, dawn_dusk_depression=18)
-
-    
-  
-    
-    # minor and major axis formater (date and time) 
-    import matplotlib.dates as dates
     
     ax[1].xaxis.set_major_formatter(dates.DateFormatter('%H:%M'))
     ax[1].xaxis.set_major_locator(dates.HourLocator(interval = 1))
     
-    ax2 = ax[1].twiny()
-    data = np.empty(len(drift))
-    data[:] = np.nan
-    index = pd.to_datetime(drift.index + pd.Timedelta(hours = -3))
-    
-    xs = pd.Series(data= data,
-                   index = index)
-    
-    xs.plot(ax = ax2)
-    
-    ax2.xaxis.set_major_formatter(dates.DateFormatter('%H:%M'))
-    ax2.xaxis.set_major_locator(dates.HourLocator(interval = 1))
- 
-   
-    ax2.xaxis.set_ticks_position('bottom') 
-    ax2.xaxis.set_label_position('bottom') 
-    ax2.spines['bottom'].set_position(('outward', 45))
+    secundary_axis(ax[1], drift)
     
     
+    # =============================================================================
+    # TERMINATOR SOLAR AT 300 KM (DUSK)
+    # =============================================================================
+    observer = astral.Observer(latitude = latitude, 
+                               longitude = longitude)
     
-    ax2.set(xlabel = "Time (LT)")
     
+    infos = sun(observer, date, 
+                dawn_dusk_depression = twilightAngle)
     
 
-
-    plt.rcParams.update({'font.size': 12, 
-                         "font.family": "Times New Roman", 
-                         })   
-  
     angles = ["sunset", "dusk"]
     linestyle = ["-", "--"]
     for num in range(2):
         
         terminator = infos[angles[num]]
         
-        ax[0].axvline(
-            terminator, color = "k", linestyle = linestyle[num], 
-                        )
         
-        ax[0].axvline(
-            terminator, color = "k", linestyle = linestyle[num], 
-                        )
+        for col in range(2):
+        
+            ax[col].axvline(
+                terminator, color = "k", linestyle = linestyle[num], 
+                            )
+            
+            ax[col].axvline(
+                terminator, color = "k", linestyle = linestyle[num], 
+                            )
         
         delta = datetime.timedelta(minutes = 5)
         
-        if angles[]
-        ax[1].text(terminator + delta,110, 
-                   "Solar terminator \nat 300 km",
-                   transform=ax[1].transData)
+        if angles[num] == "dusk":
+            text = "dusk at \n300 km".capitalize()
+        else:
+            text = angles[num].capitalize()
+            
+        ax[1].text(terminator + delta, 110, 
+                   text,
+                   transform = ax[1].transData, 
+                   fontsize = fontsize + 2)
     
+    plt.rcParams.update({'font.size': fontsize, 
+                         "font.family": "Times New Roman", 
+                         })   
 # =============================================================================
 #     SAVE
 # =============================================================================
-    
-    doy = date.timetuple().tm_yday
-    
-    if doy < 10:
-        FigureName = f"00{doy}"
-        
-    elif doy > 10 and doy < 100:
-        FigureName = f"0{doy}"
-
-    else:
-        FigureName = f"{doy}"
-    
-    
-    
-    path_out = "Figures/Fortaleza/2014/"
+  
+    FigureName = doy_str_format(date)
+    path_out = f"Figures/{site}/{date.year}/IndividualsPlots/"
     
     if save:
-        plt.savefig(f"{FigureName}.png", 
-                    dpi = 100, bbox_inches="tight")
+        plt.savefig(f"{path_out}{FigureName}.png", 
+                    dpi = 300, bbox_inches = "tight")
     
         plt.close(fig)
   
         
 def main():
-    infile = "Database/FZ_2014-2015_Processado/"
+    infile = "Database/SL_2014-2015_Processado/"
     
     _, _, files = next(os.walk(infile))
     
-    filename = files[11]
+    filename = files[0]
+    
+    print(filename)
     
     
     iono = ionosonde(infile, filename)
-    #PRE(df)    #for day in range(1, 31, 1):
+
         
-    plot(iono, 31, save = False)
+    plot(iono, 1, save = False, site = "SaoLuis")
     
 main()
