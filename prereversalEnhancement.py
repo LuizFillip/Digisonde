1b869c84dbfc5391062693e6a1d3d02b692ac7e1
LuizFillip <luizfillip@outlook.com>
2022-07-30 20:26:54 -0300

    modify the PRE functions and adding the new function for solar terminator time

--- prereversalEnhancement.py
+++ prereversalEnhancement.py
@@ -1,95 +1,66 @@
 
 from ionosonde import *
-import matplotlib.pyplot as plt
 import datetime
-#from get_time_terminator import *
-#from plotVerticaldrift import *
-
-
-
-
-
-
-
-class PRE(object):
+from sites import *
     
-    def __init__(self, data):
-        self.data = data
-    
-    
-        time = self.data.index[0]
-        year, month, day = time.year, time.month, time.day
-        
-        #try:
-           # terminator = get_time_terminator(year, month, day).terminator()
-           # time_sunset = terminator.time()
-        #except:
-        terminator = datetime.datetime(year, 
-                                       month, 
-                                       day, 
-                                       21, 45)
 
+
+def PRE(infile, filename, 
+        day = 2, delta = 1):    
+    
+   
+    df = drift(select_day(infile, 
+                          filename, 
+                          day))
+    time = df.index[0]
+    year, month, day = time.year, time.month, time.day
+       
+    dusk = terminators(filename, 
+                       date = datetime.date(year, month, day)).dusk
         
-        delta = datetime.timedelta(hours = 1)
+    df = df.loc[((df.time > (dusk - delta)) & 
+                 (df.time < (dusk + delta))), :]
+    
+    result = {}
+    
+    pre = df.max().values
+    times = df.idxmax().values
+    
+    for num, col in enumerate(df.columns[1:]):
         
-        self.data = self.data.loc[((self.data.index > terminator - delta) & 
-                                   (self.data.index < terminator + delta)), :]
+        num = num + 1
+        
+        intime = pd.to_datetime(times[num])
            
-        times = self.data.idxmax().values
-        pre = self.data.max().values
-        freqs = self.data.columns
-        
-        result = {}
-        
-        for num in range(len(times)):
-        
-            timestring = pd.to_datetime(times[num]).strftime('%H:%M')
-            
-            result[freqs[num]] = list((timestring, round(pre[num], 3)))
-        
-        #result["sunset"] = terminator.time()
-        
-        day = pd.to_datetime(times[0]).strftime('%Y-%m-%d')
-        
-        tuples = list(zip([day, day], ["Time (UT)", 
-                                       "Peak (m/s)"]))
-        
-        index = pd.MultiIndex.from_tuples(tuples, 
-                                          names=["Date", 
-                                                 "Values"])
-        
-        self.df = pd.DataFrame(result, index = index)
-        
-        self.df.columns.name = "Frequencies (MHz)"
-        
-    @property
-    def table(self):
-        return self.df
-    @property
-    def terminator(self):
-        return terminator
-    @property
-    def pre_avg(self):
-        return pre.mean()
+        result[col] = list((time_to_float(intime), 
+                            round(pre[num], 3)))
+       
+    day = pd.to_datetime(times[0]).strftime('%Y-%m-%d')
+    
+    tuples = list(zip([day, day], ["time", 
+                                   "peak"]))
+    
+    index = pd.MultiIndex.from_tuples(tuples, 
+                                      names=["Date", 
+                                             "Values"])
+    
+    df = pd.DataFrame(result, index = index)  
+    df.columns.name = site(filename).name
+    
+    return df
 
-def main():    
+def main():   
     infile = "Database/FZ_2014-2015_Processado/"
     
     _, _, files = next(os.walk(infile))
     
     filename = files[9]
     
+    pre = PRE(infile, filename, 
+              day = 2, delta = 1)
     
-    iono = ionosonde(infile, filename)
     
-    days = list(set(iono.data.index.day))
+    print(pre)
+
     
-    for day in days:
-        
-        df = vertical_drift(iono.select_day(day))
-        
-        try:
-            pre_table = PRE(df).table
-        except:
-            print(day)
-            pass
+main()
