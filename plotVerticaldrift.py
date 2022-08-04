7adc249e6d8cb00221847fd03e3c76c851254b2d
LuizFillip <luizfillip@outlook.com>
2022-08-04 14:53:29 -0300

    Ajusted the secondary axis and adding average values of PRE

--- plotVerticaldrift.py
+++ plotVerticaldrift.py
@@ -11,7 +11,7 @@ from prereversalEnhancement import *
 import astral 
 import datetime
 import matplotlib.ticker as ticker
-
+import config
 
 def get_values(infile, filename, day):
     df = PRE(infile, filename, 
@@ -26,7 +26,8 @@ def get_values(infile, filename, day):
     return (peak, time)
 
 
-def secundary_axes(ax, delta = - 3):
+
+def secondary_axis(ax, delta = - 3):
     ax1 = ax.twiny()
     
     ax1.set(xticks = ax.get_xticks(), 
@@ -36,15 +37,6 @@ def secundary_axes(ax, delta = - 3):
     ax1.xaxis.set_major_formatter(lambda x, pos: 
                                   f"%d" % (x + delta) + ":00")
     
-    for axs in [ax, ax1]:
-        
-        axs.xaxis.set_major_locator(ticker.AutoLocator())
-        axs.xaxis.set_minor_locator(ticker.AutoMinorLocator())
-        
-        axs.yaxis.set_major_locator(ticker.AutoLocator())
-        axs.yaxis.set_minor_locator(ticker.AutoMinorLocator())
-        
-    
     ax1.xaxis.set_ticks_position('bottom') 
     ax1.xaxis.set_label_position('bottom') 
     ax1.spines['bottom'].set_position(('outward', 45))
@@ -52,15 +44,18 @@ def secundary_axes(ax, delta = - 3):
 def terminator_lines(ax, filename, fontsize, year, month, day):
     
     terms = terminators(filename, 
-                       date = datetime.date(year, month, day))
+                       date = datetime.date(year, 
+                                            month, 
+                                            day))
     
     times = [terms.sunset, terms.dusk]
     altitudes = [0, 300]
     linestyle = ["-", "--"]
     kargs = dict(lw = 1, color = "k")
+    
+    
     for num in range(2):
-        
-        
+            
         for col in range(2):
         
             ax[col].axvline(
@@ -80,21 +75,7 @@ def terminator_lines(ax, filename, fontsize, year, month, day):
                    transform = ax[1].transData, 
                    fontsize = fontsize)
         
-def doy_str_format(date):
-    
-    doy = date.timetuple().tm_yday
-    
-    if doy < 10:
-        FigureName = f"00{doy}"
-        
-    elif doy > 10 and doy < 100:
-        FigureName = f"0{doy}"
 
-    else:
-        FigureName = f"{doy}"
-        
-    return FigureName
-  
     
   
 def plotVerticaldrift(infile, 
@@ -120,10 +101,13 @@ def plotVerticaldrift(infile,
     site_name = sites(filename).name
     df = select_day(infile, filename, day)
     
+    df["Avg"] = df[[6, 7, 8]].mean(axis = 1)
+    
+    
+    
     freqs = list(df.columns[1:])
     
-    colors = ["red", "black", "blue"]
-    markers = ["^", "o", "s"]
+    colors = ["red", "black", "blue", "m"]
     
     date = df.index[0]
     year, month, day = date.year, date.month, date.day
@@ -131,30 +115,29 @@ def plotVerticaldrift(infile,
 
     
     ax[0].set(ylabel = "Altitude (km)", ylim = [100, 600], 
-              title = 'F layer true heights and vertical ' \
-                  f'drift (dhF/dt)s in {site_name}, {date.date()}')
+              title = f'{site_name}, {date.date()}')
         
     ax[0].xaxis.set_major_formatter(lambda x, pos: 
                                   f"%d" % x + ":00")
         
-    ax[0].yaxis.set_major_locator(ticker.AutoLocator())
-    ax[0].yaxis.set_minor_locator(ticker.AutoMinorLocator())
     
     # =============================================================================
     # VERTICAL DRIFT SUBPLOT
     # =============================================================================
     
     vz = drift(df)
+    
+    
     args = dict(fillstyle = "none", 
-                markersize = 3, lw = 1)
+                lw = 1)
     
     for num, col in enumerate(freqs):
         ax[1].plot(vz.time, vz[col], 
                    color = colors[num], 
-                   marker = markers[num], **args)
+                    **args)
         ax[0].plot(df.time, df[col], 
                    color = colors[num], 
-                   marker = markers[num], **args)
+                    **args)
     
     ax[1].set(xlabel = "Time (UT)", 
               ylabel = "Velocity (m/s)", 
@@ -166,33 +149,28 @@ def plotVerticaldrift(infile,
                  title = "Frequencies (MHz)", 
                  ncol = 3)
     
+    # Adding an secundary axes
+    secondary_axis(ax[1], delta = - 3)
     
-    secundary_axes(ax[1], delta = - 3)
-    
-    
-    # =============================================================================
-    # TERMINATOR SOLAR AT 300 KM (DUSK)
-    # =============================================================================
+    #Plot terminator solar from date input
     terminator_lines(ax, filename, fontsize, year, month, day)
     
     
     peak, time = get_values(infile, filename, day)
     
+    peak = np.append(peak, round(vz.Avg.max(), 3))
+    time = np.append(time, vz.Avg.idxmax())
 
     for num in range(len(peak)):
         ax[1].axvline(time[num], linestyle = "-", 
                       lw = 0.5, color = colors[num])
         
         info_text = f"{freqs[num]} MHz: {peak[num]} m/s"
-        ax[1].text(18.1, 38 + (num*13), info_text, 
+        
+        ax[1].text(18.1, 28 + (num*16), info_text, 
                    color = colors[num],
                    transform = ax[1].transData)
     
-    plt.rcParams.update({'font.size': fontsize, 
-                         'mathtext.fontset': 'stix', 
-                         'font.family': 'STIXGeneral'
-                         })   
-
     site_to_save = unidecode(site_name).replace(" ", "")
     FigureName = doy_str_format(date)
     
@@ -200,7 +178,7 @@ def plotVerticaldrift(infile,
     if save:
         
         path_out = f"Figures//{site_to_save}//{date.year}//IndividualsPlots//"
-        #Figures/Fortaleza/2014/IndividualPlots
+        
         try:
             plt.savefig(f"{path_out}{FigureName}.png", 
                     dpi = 100, bbox_inches = "tight")
@@ -210,16 +188,13 @@ def plotVerticaldrift(infile,
     
         plt.close(fig)
   
-        
-def main():
-    infile = "Database/FZ_2014-2015_Processado/"
-    
-    _, _, files = next(os.walk(infile))
-    
-    filename = files[3]
-    
-    for day in range(1, 32):
-        plotVerticaldrift(infile, filename, day = day, save = True)
-    
 
-#main()
+def main():
+    
+    infile = "Database/FZ_2014-2015_Processado/"
+     
+    _, _, files = next(os.walk(infile))
+     
+    filename = files[11]
+    plotVerticaldrift(infile, filename, 
+                      day = 1, save = False)
