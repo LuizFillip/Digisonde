e890bc696e51dad2710b9903aee921ef9650040f
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2022-11-03 15:47:47 -0300

    plotting averages

--- plot/plotAnnualvariation.py
+++ plot/plotAnnualvariation.py
@@ -1,24 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jul 24 15:32:32 2022
-
-@author: Luiz
-"""
-
-import config
+from plotConfig import *
 import matplotlib.pyplot as plt
 import os
 from pipeline import *
 import matplotlib.dates as dates
-import astral 
-import datetime
-from astral.sun import sun
 import pandas as pd
-
-
-
-
-
 
 
 def plotAnnualvariation(infile, 
@@ -35,23 +20,15 @@ def plotAnnualvariation(infile,
     df = sel_parameter(infile, filename, 
                        factor = "peak")
    
-
-      
     date = df.index[0]
     
     markers = ["o", "^", "s"]
     colors = ['#FF2C00', '#845B97', '#00B945']
     
-    for num, col in enumerate(df.columns):
-        
-        
-        args = dict(linestyle = "none", 
-                    marker = markers[num],
-                    color = colors[num],
-                   # fillstyle = 'none', 
-                    markersize = 5)
-        
-        ax.plot(df.index, df[col], **args)
+    ax.plot(df.index, df.mean(axis = 1), marker = "o", 
+            linestyle = "none", color = "k", markersize = 5)
+    
+
 
     ax.legend(list(df.columns),
               title = "Frequencies (MHz)", 
@@ -66,22 +43,48 @@ def plotAnnualvariation(infile,
     ax.xaxis.set_major_formatter(dates.DateFormatter('%b'))
     ax.xaxis.set_major_locator(dates.MonthLocator(interval = 1))
     
-    path_out = f"Figures/{site}/{date.year}/Variations/"
-    FigureName = f"prereversalEnhancement_{site}_{date.year}"
-    
-    if save:
-        plt.savefig(f"{path_out}{FigureName}.png", 
-                    dpi = 100, bbox_inches="tight")
+  
  
-def main():
+
+def plotAnnualAvg():
+    year = 2014
     site = "Fortaleza"
-    infile = f"Results/{site}/PRE/"
-    filename = "2014.txt"
+    infile = f"process/{site}/PRE/"
     
-    plotAnnualvariation(infile, 
-                            filename, 
-                            site = site, 
-                            ax = None,
-                            save = False)
+    filename = f"{year}.txt"
+    
+    out = []
+    for pe in ["time", "peak"]:
+        
+    
+        out.append(sel_parameter(infile, filename, 
+                       factor = pe).mean(axis = 1))
+        
+    df = pd.concat(out, axis = 1)
+    
+    df.columns = ["time", "peak"]
+    
+    fig, ax = plt.subplots(figsize = (14, 7))
+    
+    df["peak"].plot(marker = "o", 
+                    linestyle = "none", 
+                    markersize = 10,
+                    fillstyle = "none",
+                    color = "k")
+    
+    ax.set(ylabel = "Velocidade (m/s)", 
+           xlabel = "Meses", 
+           ylim = [0, 90])
+    
+    ax.xaxis.set_major_formatter(dates.DateFormatter('%b'))
+    ax.xaxis.set_major_locator(dates.MonthLocator(interval = 1))
+    ax.tick_params(axis = 'x', labelrotation = 0)
+    
+    
+    fig.savefig(paths["latex"] + "PRE_annual_2014.png", 
+                dpi = 1000, 
+                bbox_inches = "tight")
+     
+    #df.to_csv(f"{year}.txt", index = True, sep = ",")
 
-
+main()
