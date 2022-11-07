54dd738b8a465e210528ebcfc58a2f1839751351
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2022-11-07 12:38:44 -0300

    updating

--- plot/plotVerticaldrift.py
+++ plot/plotVerticaldrift.py
@@ -1,17 +1,32 @@
 import matplotlib.pyplot as plt
-from prereversalEnhancement import PRE, drift
+#from prereversalEnhancement import PRE, get_values
 import datetime
-import matplotlib.ticker as ticker
-import plotConfig
+#from plotConfig import *
 from digisonde_utils import terminators
-from sites import coords_from_filename
-from pipeline import select_day
-import unicodedata
+from sites import infos_from_filename
+from pipeline import iono_frame, drift
 import numpy as np
+import os
 
 
+"""
 
+def text_peaks(infile, filename, day):
+    peak, time = get_values(infile, filename, day)
+    
+    peak = np.append(peak, round(vz.Avg.max(), 3))
+    time = np.append(time, vz.Avg.idxmax())
 
+    for num in range(len(peak)):
+        ax[1].axvline(time[num], linestyle = "-", 
+                      lw = 0.5, color = colors[num])
+        
+        info_text = f"{freqs[num]} MHz: {peak[num]} m/s"
+        
+        ax[1].text(18.1, 28 + (num*16), info_text, 
+                   color = colors[num],
+                   transform = ax[1].transData)
+"""
 
 def secondary_axis(ax, delta = - 3):
     ax1 = ax.twiny()
@@ -25,9 +40,9 @@ def secondary_axis(ax, delta = - 3):
     
     ax1.xaxis.set_ticks_position('bottom') 
     ax1.xaxis.set_label_position('bottom') 
-    ax1.spines['bottom'].set_position(('outward', 45))
+    ax1.spines['bottom'].set_position(('outward', 55))
 
-def terminator_lines(ax, filename, fontsize, year, month, day):
+def terminator_lines(ax, filename, year, month, day):
     
     terms = terminators(filename, 
                        date = datetime.date(year, 
@@ -56,10 +71,9 @@ def terminator_lines(ax, filename, fontsize, year, month, day):
         
         text = f"Terminator \n at {altitudes[num]} km"
             
-        ax[1].text(times[num] + 0.1, 110, 
+        ax[1].text(times[num] + 0.1, 70, 
                    text,
-                   transform = ax[1].transData, 
-                   fontsize = fontsize)
+                   transform = ax[1].transData)
         
 
     
@@ -67,25 +81,20 @@ def terminator_lines(ax, filename, fontsize, year, month, day):
 def plotVerticaldrift(infile, 
                       filename,
                       day = 1, 
-                      fontsize = 14,
-                      save = True):
-    
-    if save: 
-        plt.ioff()
+                      fontsize = 10):
 
-    fig, ax = plt.subplots(figsize = (10, 6), 
-                           nrows = 2, sharex = True)
+    fig, ax = plt.subplots(figsize = (15, 10), 
+                           nrows = 2, 
+                           sharex = True)
     
     plt.style.use('seaborn-talk')
     
     plt.subplots_adjust(hspace = 0)
+    name = "Fortaleza"
+    df = iono_frame(infile, filename)
     
-    name, lat, lon = coords_from_filename(filename)
-    df = select_day(infile, filename, day)
+    df = df.sel_day_in(day = day)
     
-    df["Avg"] = df[[6, 7, 8]].mean(axis = 1)
-    
-
     freqs = list(df.columns[1:])
     
     colors = ["red", "black", "blue", "m"]
@@ -106,7 +115,7 @@ def plotVerticaldrift(infile,
     
     
     args = dict(fillstyle = "none", 
-                lw = 1)
+                lw = 2)
     
     for num, col in enumerate(freqs):
         ax[1].plot(vz.time, vz[col], 
@@ -118,11 +127,10 @@ def plotVerticaldrift(infile,
     
     ax[1].set(xlabel = "Time (UT)", 
               ylabel = "Velocity (m/s)", 
-              ylim = [-90, 90], 
+              ylim = [-50, 50], 
               xlim = [18, 24])
     
     ax[0].legend(freqs, loc = 'upper left', 
-                 prop={'size': fontsize - 2},
                  title = "Frequencies (MHz)", 
                  ncol = 3)
     
@@ -130,49 +138,23 @@ def plotVerticaldrift(infile,
     secondary_axis(ax[1], delta = - 3)
     
     #Plot terminator solar from date input
-    terminator_lines(ax, filename, fontsize, year, month, day)
+    terminator_lines(ax, filename, year, month, day)
     
     
-    peak, time = get_values(infile, filename, day)
     
-    peak = np.append(peak, round(vz.Avg.max(), 3))
-    time = np.append(time, vz.Avg.idxmax())
-
-    for num in range(len(peak)):
-        ax[1].axvline(time[num], linestyle = "-", 
-                      lw = 0.5, color = colors[num])
-        
-        info_text = f"{freqs[num]} MHz: {peak[num]} m/s"
-        
-        ax[1].text(18.1, 28 + (num*16), info_text, 
-                   color = colors[num],
-                   transform = ax[1].transData)
     
-    site_to_save = unidecode(name).replace(" ", "")
-    FigureName = date.strftime("%j")
-    
-    if save:
-        
-        path_out = f"Figures//{site_to_save}//{date.year}//IndividualsPlots//"
-        
-        try:
-            plt.savefig(f"{path_out}{FigureName}.png", 
-                    dpi = 100, bbox_inches = "tight")
-        except:
-            plt.savefig(f"{FigureName}.png", 
-                    dpi = 100, bbox_inches = "tight")
-    
-        plt.close(fig)
-  
+    return fig
 
 def main():
     
-    infile = "Database/FZ_2014-2015_Processado/"
-     
+    infile = "database/process/"
+
     _, _, files = next(os.walk(infile))
-     
-    filename = files[11]
-    plotVerticaldrift(infile, filename, 
-                      day = 2, save = False)
+
+    filename = files[0]
+
+    fig = plotVerticaldrift(infile, 
+                            filename, 
+                            day = 1)
 
 main()
