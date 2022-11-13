b01001c9d2471a42e9057e4597d7544c64607a89
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2022-11-13 18:41:06 -0300

    Adding shade for ESF period and labeling

--- plot/plotVerticaldrift.py
+++ plot/plotVerticaldrift.py
@@ -1,46 +1,27 @@
 import matplotlib.pyplot as plt
-#from prereversalEnhancement import PRE, get_values
+from PRE import drift
 import datetime
-#from plotConfig import *
+from plotConfig import path_tex
 from digisonde_utils import terminators
-from sites import infos_from_filename
-from pipeline import iono_frame, drift
+from pipeline import iono_frame
 import numpy as np
 import os
 
 
-"""
-
-def text_peaks(infile, filename, day):
-    peak, time = get_values(infile, filename, day)
-    
-    peak = np.append(peak, round(vz.Avg.max(), 3))
-    time = np.append(time, vz.Avg.idxmax())
-
-    for num in range(len(peak)):
-        ax[1].axvline(time[num], linestyle = "-", 
-                      lw = 0.5, color = colors[num])
-        
-        info_text = f"{freqs[num]} MHz: {peak[num]} m/s"
-        
-        ax[1].text(18.1, 28 + (num*16), info_text, 
-                   color = colors[num],
-                   transform = ax[1].transData)
-"""
 
 def secondary_axis(ax, delta = - 3):
     ax1 = ax.twiny()
     
     ax1.set(xticks = ax.get_xticks(), 
-            xlabel = "Time (LT)", 
+            xlabel = "Tempo (LT)", 
             xlim = ax.get_xlim())
     
     ax1.xaxis.set_major_formatter(lambda x, pos: 
-                                  f"%d" % (x + delta) + ":00")
+                                  "%d" % (x + delta) + ":00")
     
     ax1.xaxis.set_ticks_position('bottom') 
     ax1.xaxis.set_label_position('bottom') 
-    ax1.spines['bottom'].set_position(('outward', 55))
+    ax1.spines['bottom'].set_position(('outward', 130))
 
 def terminator_lines(ax, filename, year, month, day):
     
@@ -52,9 +33,9 @@ def terminator_lines(ax, filename, year, month, day):
     times = [terms.sunset, terms.dusk]
     altitudes = [0, 300]
     linestyle = ["-", "--"]
-    kargs = dict(lw = 1, color = "k")
+    kargs = dict(lw = 2, color = "k")
     
-    
+    letter = ["(a)", "(b)"]
     for num in range(2):
             
         for col in range(2):
@@ -69,11 +50,16 @@ def terminator_lines(ax, filename, year, month, day):
                     linestyle = linestyle[num], 
                     **kargs)
         
-        text = f"Terminator \n at {altitudes[num]} km"
+        text = f"Terminadouro \n em {altitudes[num]} km"
             
-        ax[1].text(times[num] + 0.1, 70, 
-                   text,
-                   transform = ax[1].transData)
+        ax[0].text(times[num] + 0.1, 210, 
+                   text, 
+                   transform = ax[0].transData)
+        
+        
+        ax[num].text(0.02, 0.9, 
+                     letter[num], 
+                     transform = ax[num].transAxes)
         
 
     
@@ -81,68 +67,67 @@ def terminator_lines(ax, filename, year, month, day):
 def plotVerticaldrift(infile, 
                       filename,
                       day = 1, 
-                      fontsize = 10):
+                      name = "Fortaleza"):
 
-    fig, ax = plt.subplots(figsize = (15, 10), 
+    fig, ax = plt.subplots(figsize = (25, 20), 
                            nrows = 2, 
                            sharex = True)
     
-    plt.style.use('seaborn-talk')
     
-    plt.subplots_adjust(hspace = 0)
-    name = "Fortaleza"
+    plt.subplots_adjust(hspace = 0.1)
+    
     df = iono_frame(infile, filename)
     
     df = df.sel_day_in(day = day)
     
     freqs = list(df.columns[1:])
     
-    colors = ["red", "black", "blue", "m"]
-    
+  
     date = df.index[0]
     year, month, day = date.year, date.month, date.day
     
 
     
-    ax[0].set(ylabel = "Altitude (km)", ylim = [100, 600], 
-              title = f'{name}, {date.date()}')
+    ax[0].set(ylabel = "Altitude (km)", 
+              ylim = [200, 400], 
+              )
         
     ax[0].xaxis.set_major_formatter(lambda x, pos: 
-                                  f"%d" % x + ":00")
+                                   "%d" % x + ":00")
         
     
     vz = drift(df)
     
+    start = 22.10
+    end = 24.0
+    
+    ax[1].axvspan(start, end, alpha = 0.5, color = "gray")
+    ax[1].text(start + 0.7, 30, "ESF", transform = ax[1].transData)
     
     args = dict(fillstyle = "none", 
-                lw = 2)
+                lw = 3)
     
     for num, col in enumerate(freqs):
-        ax[1].plot(vz.time, vz[col], 
-                   color = colors[num], 
+        ax[1].plot(vz.time, vz[col],
                     **args)
         ax[0].plot(df.time, df[col], 
-                   color = colors[num], 
                     **args)
-    
-    ax[1].set(xlabel = "Time (UT)", 
-              ylabel = "Velocity (m/s)", 
+    ax[1].set(xlabel = "Tempo (UT)", 
+              ylabel = r"$V_z$ (m/s)", 
               ylim = [-50, 50], 
+              yticks = np.arange(-40, 45, 10),
               xlim = [18, 24])
     
-    ax[0].legend(freqs, loc = 'upper left', 
-                 title = "Frequencies (MHz)", 
+    ax[0].legend(freqs, 
+                 loc = 'lower left',
+                 title = "Frequências (MHz)", 
                  ncol = 3)
     
-    # Adding an secundary axes
     secondary_axis(ax[1], delta = - 3)
-    
-    #Plot terminator solar from date input
     terminator_lines(ax, filename, year, month, day)
     
-    
-    
-    
+    fig.suptitle(f'{name}, {date.date()}', y = 0.91)
+   
     return fig
 
 def main():
@@ -152,9 +137,13 @@ def main():
     _, _, files = next(os.walk(infile))
 
     filename = files[0]
+    
 
     fig = plotVerticaldrift(infile, 
                             filename, 
                             day = 1)
+
+    fig.savefig(path_tex["latex"] + "vz_and_heights.png", 
+               dpi = 500)
 
 main()
