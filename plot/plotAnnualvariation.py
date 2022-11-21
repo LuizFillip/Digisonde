52373e4182f43955e1474642e766944d40f8094e
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2022-11-21 17:23:11 -0300

    updting

--- plot/plotAnnualvariation.py
+++ plot/plotAnnualvariation.py
@@ -16,16 +16,16 @@ def tex_path(folder):
     return os.path.join(latex, folder)
 
 
-def plotAnnualAvg():
+def plotAnnualAvg(year = 2014):
     infile = "database/FZ_PRE_2014_2015.txt"
     
     df = pd.read_csv(infile, index_col = 0)
     
     df.index = pd.to_datetime(df.index)
     
-    df = df.loc[df.index.year == 2014]
+    df = df.loc[df.index.year == year]
     
-    fig, ax = plt.subplots(figsize = (22, 10))
+    fig, ax = plt.subplots(figsize = (25, 10))
     
     df["vz"].plot(marker = "o", 
                     markersize = 15,
@@ -36,12 +36,14 @@ def plotAnnualAvg():
            xlabel = "Meses", 
            ylim = [0, 90])
     
+    ax.text(0.01, 0.9, year, transform = ax.transAxes)
+    
     ax.xaxis.set_major_formatter(dates.DateFormatter('%b'))
     ax.xaxis.set_major_locator(dates.MonthLocator(interval = 1))
     ax.tick_params(axis = 'x', labelrotation = 0)
     
     name = tex_path("results\\PRE_annual_2014.png")
-    fig.savefig(name, dpi = 400)
+    fig.savefig(name, dpi = 300)
      
     #df.to_csv(f"{year}.txt", index = True, sep = ",")
 plotAnnualAvg()
