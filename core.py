707d7986671ebcce931ba92e498e4ce855a07a0a
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-01-28 13:59:38 -0300

    removing extra kwords input

--- core.py
+++ core.py
@@ -1,19 +1,17 @@
 import pandas as pd
 import numpy as np
-import os
 import datetime
-from unidecode import unidecode
 pd.options.mode.chained_assignment = None
 
 
 
-def find_header(infile:str, 
-                filename: str, 
-                header: str = 'yyyy.MM.dd') -> tuple:
+def find_header(infile: str, 
+                header: str = 'yyyy.MM.dd'
+                ) -> tuple:
     
-    """Function for find the header and the data body"""
+    """Function for find the header and the data section"""
     
-    with open(infile + filename) as f:
+    with open(infile) as f:
         data = [line.strip() for line in f.readlines()]
     
     count = 0
@@ -22,8 +20,7 @@ def find_header(infile:str,
             break
         else:
             count += 1
-    
-    
+
     data_ = data[count + 2:]
     
     header_ = data[1].split()
@@ -49,9 +46,11 @@ def time_to_float(intime) -> float:
             
             
         elif ":" in intime:
-            time_list = [int(num) for num in str(intime).split(":")]
+            time_list = [int(num) for num 
+                         in str(intime).split(":")]
     except:
-        raise TypeError("The input parameter must be datetime.datime or clock format")
+        raise TypeError("The input parameter" + 
+                        "must be datetime.datime or clock format")
         
     return round(time_list[0] + 
                  (time_list[1] / 60) + 
@@ -94,23 +93,24 @@ class iono_frame(object):
     with datetime index, frequencies values in float format
     """
     
-    def __init__(self, 
-                 infile: str, 
-                   filename: str, 
-                   day: int = 1,
-                   columns: list = ["time", 6, 7, 8], 
-                   ) -> pd.DataFrame:
+    def __init__(
+            self, 
+            infile: str,  
+            day: int = 1,
+            columns: list = ["time", 6, 7, 8], 
+            ) -> pd.DataFrame:
     
         self.columns = columns
     
-        header, data = find_header(infile, filename)
+        header, data = find_header(infile)
         
         df = pd.DataFrame(structure_the_data(data), 
                           columns = header)
         
         df.rename(columns = {"yyyy.MM.dd": "date", 
                              "HH:mm:ss": "time", 
-                             "(DDD)": "doy"}, inplace = True)
+                             "(DDD)": "doy"}, 
+                  inplace = True)
         
         df.index = pd.to_datetime(df["date"] + 
                                   " " +
@@ -126,8 +126,10 @@ class iono_frame(object):
                       inplace = True)
             
             df[name] = df[name].apply(pd.to_numeric, 
-                                              errors='coerce')
-        df.drop(columns = {"date", "doy"}, inplace = True) 
+                                      errors='coerce')
+            
+        df.drop(columns = {"date", "doy"}, 
+                inplace = True) 
         
         self.df = df
         
@@ -135,22 +137,21 @@ class iono_frame(object):
         
         
     def sel_day(self, day = 1):
+        """Select one single day"""
         return self.df.loc[self.df.index.day == day, self.columns]
     
     def sel_day_in(self, 
                    day = 1, 
                    begin_time: str = '18:00:00', 
                    end_time: str = '23:50:00'):
+        """Select time range in day"""
         return self.sel_day(day).between_time(begin_time, end_time)
 
     
     
-   
-
-
-def sel_parameter(infile, filename, factor = "peak"):
+def sel_parameter(infile, factor = "peak"):
     
-    df = pd.read_csv(infile + filename, 
+    df = pd.read_csv(infile, 
                      delim_whitespace=(True), 
                      index_col = [0, 1])
     
@@ -158,4 +159,14 @@ def sel_parameter(infile, filename, factor = "peak"):
     
     df.index = pd.to_datetime(df.index.get_level_values(0))
     return df
+
+
+def main():
+
+    infile = "database/process/SL_2014-2015/SAA0K_201401.txt"
+    
+    df = iono_frame(infile).sel_day()
+    
+    print(df)
+
 
