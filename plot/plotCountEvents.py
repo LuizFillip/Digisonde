66cdba9447a6be2add2ce4d998eb28719c56240a
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-01-30 13:35:08 -0300

    join into the func

--- plot/plotCountEvents.py
+++ plot/plotCountEvents.py
@@ -2,34 +2,44 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import setup as s
+import os
 
-site = "Sao luis"
+infile = "database/counts/"
 
-name = site.replace(" ", "_")
-infile = f"database/counts/{name}_2013_2023.txt"
 
-fig, ax = plt.subplots(figsize = (10, 4))
+_, _, files = next(os.walk(infile))
 
-s.config_labels()
+filename = files[1]
 
-df = pd.read_csv(infile, index_col = 0)
 
-img = ax.imshow(df.values, 
-                aspect = "auto", 
-                cmap = "Blues", 
-                vmax = 144)
-
-ax.set_xticks(np.arange(0, 365, 30))
-
-ax.set_yticks(np.arange(len(df.index)), 
-              labels = df.index)
-
-ax.set(ylabel = "Anos", 
-       xlabel = "Dias", 
-       title = f"Velocidade de deriva - {site}")
-
-s.colorbar_setting(img, ax, 
-                   ticks = np.arange(0, 150, 20),
-                   label = "# dados por dia")
-
-plt.show()
+def plot(infile, filename):
+    args = filename.split("_")
+    ystart = int(args[1])
+    yend = int(args[-1].replace(".txt", ""))
+    
+    fig, ax = plt.subplots(figsize = (10, 4))
+    
+    s.config_labels()
+    
+    df = pd.read_csv(infile + filename, index_col = 0)
+    
+    img = ax.pcolormesh(df.columns, 
+                        df.index, 
+                        df.values, 
+                        cmap = "Blues", 
+                        vmax = 144)
+    
+    ax.set_xticks(np.arange(0, 365, 30))
+    
+    
+    ax.set(ylabel = "Anos", 
+           xlabel = "Dias", 
+           yticks = list(df.index),
+           ylim = [ystart, yend],
+           title = f"Velocidade de deriva - {args[0]}")
+    
+    s.colorbar_setting(img, ax, 
+                       ticks = np.arange(0, 150, 20),
+                       label = "# dados por dia")
+    
+    plt.show()
