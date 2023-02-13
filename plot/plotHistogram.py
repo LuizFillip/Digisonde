76de7a8675a166cc32d80b4f1e6f8f422b13ae08
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-02-13 13:39:24 -0300

    plotting from array values

--- plot/plotHistogram.py
+++ plot/plotHistogram.py
@@ -2,30 +2,35 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plotHistogram(year = 2015, col= "vzp"):
-    
+def load( year = 2015, col= "vzp"):
     infile = f"database/drift/{year}.txt"
 
     df = pd.read_csv(infile, index_col = 0)
 
     df.index = pd.to_datetime(df.index)
-    
+    return df
+
+
+def plotHistogram(arr):
+
     fig, ax = plt.subplots(figsize = (6, 4))
 
-    lmax = round(df.max().max())
-    lmin = round(df.min().min())
+    lmax = round(arr.max())
+    lmin = round(arr.min())
 
-    binwidth = 5
+    binwidth = 10
 
     bins = np.arange(lmin, 
                      lmax + binwidth, 
                      binwidth)
+    
+    
 
     ax.set(title = "Velocidade do PRE", 
            xlabel = "Velocidade (m/s)",
            ylabel = "Número de eventos",
            xlim = [lmin - binwidth, 
-                    lmax + binwidth])
+                   lmax + binwidth])
 
     args = dict(facecolor = 'lightgrey', 
                 alpha = 1, 
@@ -35,12 +40,11 @@ def plotHistogram(year = 2015, col= "vzp"):
                 linewidth = 1)
 
 
-    df[col].hist(bins = bins, 
-                   ax = ax, **args)
+    ax.hist(arr, bins = bins, **args)
      
-    plot_stats(ax, df[col])
+    plot_stats(ax, arr)
     
-    plt.show()
+    return fig
     
     
 def plot_stats(ax, data, unit = "m/s"):
@@ -50,11 +54,32 @@ def plot_stats(ax, data, unit = "m/s"):
     info_mean = f"$<V_z> = {mean}$ {unit}\n"
     info_std = f"  $\sigma = {std}$ {unit}"
     
-    ax.text(0.4, 0.7, info_mean + info_std, 
+    ax.text(0.05, 0.7, info_mean + info_std, 
             fontsize = 15, 
             transform = ax.transAxes)
     
    
-plotHistogram(year = 2019)
+#
     
+from Digisonde.statistical import load_drift, get_month_avg
+
+df = load_drift()
+
+
+col = "vz"
+
+vz_limits = (-100, 100)
+
+
+cond_limits = ((df[col] > vz_limits[0]) &  
+             (df[col] < vz_limits[-1]))
+
+
+df = df.loc[(df.index.month == 1) & cond_limits]
+
+
+
+arr = df["vx"].values
+
+plotHistogram(arr)
     
