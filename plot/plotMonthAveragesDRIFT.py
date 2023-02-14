9230183970b810e2f0b942bf2b3d4cf4199dbb2f
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-02-14 10:52:05 -0300

    updating month avg

--- plot/plotMonthAveragesDRIFT.py
+++ plot/plotMonthAveragesDRIFT.py
@@ -4,14 +4,38 @@ from Digisonde.statistical import load_drift, get_month_avg
 import numpy as np
 
 
+def single_plot(ax, n, site, ext, col, smoothed):
+    
+    df = load_drift(n, 
+                    site = site, 
+                    ext = ext, 
+                    smoothed = smoothed)
+    
+    mon_str = df.index[0].strftime("%B")
+    
+    ax.set(title = mon_str,
+           xticks = np.arange(0, 24, 3))
+    
+    ax.axhline(0, linestyle = "--", color = "r")
+    
+    df1 = get_month_avg(df, col = col)
+
+    df1 = df1.reindex(np.arange(0, 24, 0.5), 
+                      method = "nearest")
+    
+    args = dict(color = "k", capsize = 2)
+
+    ax.errorbar(df1.index, 
+                df1.mean(axis = 1), 
+                yerr = df1.std(axis = 1), 
+                **args)
+    return ax
+
 def plotSeasonalDRIFT(site = "SSA", 
                       ext = "PRO", 
-                      col = "vz"):
+                      col = "vz", 
+                      year = "2013"):
     
-    dates = pd.date_range("2013-1-1", 
-                          "2013-12-31", 
-                          freq = "1M")
-   
     
     fig, ax = plt.subplots(nrows = 3, 
                            ncols = 4, 
@@ -41,33 +65,35 @@ def plotSeasonalDRIFT(site = "SSA",
         
     for n, ax in enumerate(ax.flatten(order = "F")):
         
-        mon_str = dates[n].strftime("%B")
         
-        ax.set(title = mon_str,
-               xticks = np.arange(0, 24, 3))
+        single_plot(ax, n, site, ext, col)
         
-        ax.axhline(0, linestyle = "--", color = "r")
+        ax.set_ylim(ylim)
         
-        df = load_drift(n, site = site, ext = ext)
         
-        df1 = get_month_avg(df, col = col)
         
-        new_idx = pd.Index(np.arange(0, 24, 0.5))
-        
-        df1 = df1.reindex(new_idx, method = "nearest")
-        
-        args = dict(color = "k", capsize = 2)
-    
-        ax.errorbar(df1.index, 
-                    df1.mean(axis = 1), 
-                    yerr = df1.std(axis = 1), 
-                    **args)
-        
-    fig.suptitle(f"{name} - São Luis - 2013")
+    fig.suptitle(f"{name} - São Luis - {year}")
     
     
     return fig
 
 
 
-plotSeasonalDRIFT(col = "vx", ext = "PRO")
+
+
+
+
+#plotSeasonalDRIFT(**args)
+def main():
+    n = 9
+    fig, ax = plt.subplots()
+    
+    colors = ["blue", "k"]
+    
+    for m in [False, True]:
+        args = dict(
+            site = "SSA", 
+            ext = "RAW", 
+            col = "vx", smoothed = m
+                    )
+        single_plot(ax, n, **args)
