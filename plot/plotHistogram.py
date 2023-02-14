ffadc62c420a88dc75864e0f005a3ac32417f968
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-02-14 11:34:38 -0300

    subplots histograma]

--- plot/plotHistogram.py
+++ plot/plotHistogram.py
@@ -1,34 +1,25 @@
-import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-
-def load( year = 2015, col= "vzp"):
-    infile = f"database/drift/{year}.txt"
-
-    df = pd.read_csv(infile, index_col = 0)
-
-    df.index = pd.to_datetime(df.index)
-    return df
+from Digisonde.statistical import load_drift, get_month_avg
 
 
-def plotHistogram(arr, title):
 
-    fig, ax = plt.subplots(figsize = (6, 4))
+
+def plotHistogram(ax, arr, binwidth = 10):
 
     lmax = round(arr.max())
     lmin = round(arr.min())
-
-    binwidth = 10
 
     bins = np.arange(lmin, 
                      lmax + binwidth, 
                      binwidth)
 
-    ax.set(title = title, 
-           xlabel = "Velocidade (m/s)",
-           ylabel = "Número de eventos",
+    ax.set(
+           #xlabel = "Velocidade (m/s)",
+           #ylabel = "Número de eventos",
            xlim = [lmin - binwidth, 
-                   lmax + binwidth])
+                   lmax + binwidth]
+           )
 
     args = dict(facecolor = 'lightgrey', 
                 alpha = 1, 
@@ -42,10 +33,10 @@ def plotHistogram(arr, title):
      
     plot_stats(ax, arr)
     
-    return fig
+    return ax
     
     
-def plot_stats(ax, arr, unit = "m/s"):
+def plot_stats(ax, arr, unit = "m/s", fontsize = 15):
     mean = round(arr.mean(), 2)
     std = round(arr.std(), 2)
     vmax = round(arr.max(), 2)
@@ -58,42 +49,37 @@ def plot_stats(ax, arr, unit = "m/s"):
     info_min = f"min = {vmin} {unit}"
     
     
-    ax.text(0.05, 0.7, (info_mean + 
+    ax.text(0.05, 0.6, (info_mean + 
                         info_std + 
                         info_max + 
                         info_min), 
-            fontsize = 15, 
+            fontsize = fontsize, 
             transform = ax.transAxes)
     
    
-#
+
+def HistSubplots():
+    args = dict(
+        site = "SSA", 
+        ext = "RAW", 
+        smoothed = True
+                )
     
-from Digisonde.statistical import load_drift, get_month_avg
-
-df = load_drift()
-
-col = "vz"
-
-if col == "vz":
-    name = "Deriva vertical"
-    ylim = [-50, 50]
+    fig, ax = plt.subplots(nrows = 3, 
+                           ncols = 4, 
+                           sharex = True, 
+                           sharey = True, 
+                           figsize = (22, 12))
     
-elif col == "vx":
-    name = "Deriva meridional"
-    ylim = [-150, 150]
-else:
-    name = "Deriva zonal"
-    ylim = [-150, 150]
-vz_limits = (-100, 100)
-
-
-cond_limits = ((df[col] > vz_limits[0]) &  
-               (df[col] < vz_limits[-1]))
-
-
-df = df.loc[(df.index.month == 2) & cond_limits]
-
-arr = df[col].values
-
-plotHistogram(arr, title = name)
+    col = "vz"
     
+    plt.subplots_adjust(wspace = 0.1)
+    for n, ax in enumerate(ax.flat):
+    
+        df = load_drift(n, **args)
+        
+        arr = df[col].values
+        title = df.index[0].strftime("%B")
+        ax = plotHistogram(ax, arr, binwidth = 10)
+        
+        ax.set(title = title)
