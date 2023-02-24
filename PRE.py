a820a94a313108d4f379e17db8e7db886ca75e82
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-02-24 12:02:03 -0300

    general equation for Vzp

--- PRE.py
+++ PRE.py
@@ -76,5 +76,16 @@ def all_frequencies(df):
                                              "values"])
     return pd.DataFrame(dat, index = index) 
 
-
+def general_vz(vz, uy, wd, i):
+    """
+    A general expression for vertical plasma drift including
+    the effect from meridional wind and plasma diffusion
+    """
+    I = np.radians(i)
+    
+    vz_term = vz * np.cos(I)
+    uy_term = uy * np.cos(I) * np.sin(I)
+    wd_term = wd * pow(np.sin(I), 2)
+    
+    return vz_term + uy_term - wd_term 
 
