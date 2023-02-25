2a1373cfa7a982139fcad3f719bb0d4dd31cdea0
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-02-25 16:07:48 -0300

    removing get avg

--- drift_utils.py
+++ drift_utils.py
@@ -1,12 +1,11 @@
 from build import paths as p
 import pandas as pd
 import numpy as np
-import os
-from Digisonde.drift import load_export
-from Digisonde.utils import smooth
-pd.options.mode.chained_assignment = None
+from Digisonde.utils import smooth, time2float
 
-def load_drift(n, 
+#pd.options.mode.chained_assignment = None
+
+def load_drift(n = None, 
                site = "SSA", 
                ext = "RAW", 
                smoothed = True, 
@@ -32,53 +31,6 @@ def load_drift(n,
     else:
         return df.loc[df.index.month == n]
 
-
-def get_month_avg(df, col = "vz", 
-                  sample = "5min"):
-
-    df = df.resample(sample).last().dropna()
-    
-    df["time"] = (df.index.hour + 
-                  df.index.minute / 60)
-
-    days = np.unique(df.index.day)
-    
-    out = []
-    
-    for day in days:
-        df1 = df.loc[df.index.day == day, :]
-        
-        df1.rename(columns = {col: day}, 
-                   inplace = True)
-        df1.index = df1["time"]
-        out.append(df1[day])
-    
-    return pd.concat(out, axis = 1)
-    
-
-
-def concat_files(site = "SSA_PRO", 
-                 save = True):
-
-    f = p("Drift")
-    
-    files = f.get_files_in_dir(site)
-    out = []
-    for filename in files:
-        out.append(load_export(filename))
-    
-    df = pd.concat(out).sort_index()
-    
-    if save:
-    
-        to_save = os.path.join(f.root,
-                               site[:3], 
-                               "PRO_2013.txt")
-
-        df.to_csv(to_save, index = True)
-        
-    return df
-
 def filter_values(avg, std, df, std_factor = 1):
     
     out = []
@@ -100,3 +52,26 @@ def filter_values(avg, std, df, std_factor = 1):
                                 index = df.index))
         
     return pd.concat(out, axis = 1)
+
+
+def pivot_data(n, col = "vx"):
+
+    df = load_drift(n, smoothed = False)
+    
+    df["time"] = time2float(df.index.time)
+        
+    return pd.pivot_table(
+        df, 
+        values = col, 
+        columns = df.index.date, 
+        index = "time"
+        )
+
+
+def get_avg_std(df):
+    return df.mean(axis = 1), df.std(axis = 1)
+
+df = load_drift(1, ext = "PRO")
+
+
+print(df)
