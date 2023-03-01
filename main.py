194737abd4b2688c11290ee211a456001cee2e16
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-03-01 12:03:33 -0300

    concat files (like str) and save it

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
 import os
-from core import iono_frame
 from tqdm import tqdm 
-from PRE import pre
 import pandas as pd
 from Digisonde.drift import load_export
 from build import paths as p
+from Digisonde.drift_utils import process_year
+
+
+
+def concat_files_and_save(infile,
+                          ext = "DVL", 
+                          filename = "2015.txt") -> str:
+    #infile = "database/Drift/RAW/"
+    files = os.listdir(infile)
+    files = [f for f in files if f.endswith(ext)]
+    out = []
+    for filename in tqdm(files, 
+                         desc = "run"):
+        try:
+            f = open(infile + filename, "r")
+            out.append(f.read())
+        except:
+            continue
+        
+    res = "".join(out)
+    
+    text_file = open(filename, "w")
+    text_file.write(res)
+    text_file.close()
+    return res
 
 def concat_files(site = "SSA_PRO", 
-                 save = True):
+                 save = True, 
+                 year = 2015):
     
     """
     load and concat all files prosseced 
@@ -18,74 +42,31 @@ def concat_files(site = "SSA_PRO",
     
     files = f.get_files_in_dir(site)
     out = []
-    for filename in files:
+    for filename in tqdm(files, desc = "running"):
         out.append(load_export(filename))
     
     df = pd.concat(out).sort_index()
     
     if save:
         #filename variable missing
-        to_save = os.path.join(f.root,
-                               site[:3], 
-                               "PRO_2013.txt") 
-
+        to_save = os.path.join(f.root, f"{year}.txt") 
         df.to_csv(to_save, index = True)
         
     return df
 
-def run_for_all_files(infile):
-    _, _, files = next(os.walk(infile))
     
+def process_dirs():
     out = []
-    for filename in files:
-        days = iono_frame(infile + filename).days
-        for day in tqdm(days, desc = filename):
-            try:
-                out.append(pre(infile, filename, day = day))
-            except:
-                continue
-
-    return pd.concat(out)           
-
-def compute_pre(station = "SL", save = True):
+    for name in ["vy", "vx", "vz"]:
+        out.append(process_year(name))
+        
+    ts = pd.concat(out, axis = 1)
     
+    f = p("Drift").get_dir("REDUCED")
     
-    infile = f"database/process/{station}_2014-2015/"
-    
-    df = run_for_all_files(infile)
-    
-    if save:
-        df.to_csv(f"database/vzp/{station}_PRE_2014_2015.txt", 
-                  sep = ",", 
-                  index = True)
-    return df
+    ts.to_csv(os.path.join(f, "2013.txt"))
+    return ts
 
 
-def join_drift_files(root,
-                     year = 2013, 
-                     site = "SAA", 
-                     save = True):
-    
-    base = f"{root}{site}\\{year}\\"
-    _, folders, _ = next(os.walk(base))
-    
-    from drift import process_day
-    
-    out = []
-    
-    for folder in folders:
-        try:
-            print("process...", folder)
-            out.append(process_day(os.path.join(base, folder)))
-        except:
-            continue
-    
-    df = pd.concat(out)
-    
-    if save:
-    
-        df.to_csv(f"{year}_raw.txt", 
-                  index = True)
-    return df
 
-    
+#concat_files(site = "RAW",  save = True)
