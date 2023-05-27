c2070801fde5eced1ea9975178171a9aaf28e261
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-05-27 18:13:22 -0300

    new plot for profilogram

--- src/plotting/plot_profilogram.py
+++ src/plotting/plot_profilogram.py
@@ -1,7 +1,11 @@
 import matplotlib.pyplot as plt
+import pandas as pd
+import settings as s
+import numpy as np
 
-
-def plot_profilogram(df):
+def plot_profilogram(
+        ax, df
+        ):
 
 
     ds = pd.pivot_table(
@@ -11,8 +15,36 @@ def plot_profilogram(df):
         columns = df.index
         ).interpolate()
     
-    plt.contourf(ds.columns, ds.index, ds.values, 
-                 50, cmap = "jet")
+    img = ax.contourf(ds.columns, ds.index, ds.values, 
+                 50, cmap = "Blues")
     
-    plt.show()
+    ticks = np.linspace(np.nanmin(ds.values), 
+                        np.nanmax(ds.values), 10)
+    s.colorbar_setting(
+            img, ax, ticks, 
+            label = 'Frequência (MHz)')
     
+    ax.set(ylabel = "Altura (km)")
+
+
+infile = "database/Digisonde/SAA0K_20130316(075).TXT"
+df = pd.read_csv(infile, index_col = 0)
+df.index = pd.to_datetime(df.index)
+times = df.index.unique()
+
+
+
+fig, ax = plt.subplots(
+    dpi = 300,
+    figsize = (12, 4)
+    )
+
+s.config_labels()
+
+plot_profilogram(ax, df)
+
+s.format_time_axes(
+        ax, hour_locator = 12, 
+        day_locator = 1, 
+        tz = "UTC"
+        )
