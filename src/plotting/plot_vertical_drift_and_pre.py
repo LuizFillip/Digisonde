6386000193603bca6ca99fad171a789ea698d0e2
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-06-20 06:21:07 -0300

    removing save_plots

--- src/plotting/plot_vertical_drift_and_pre.py
+++ src/plotting/plot_vertical_drift_and_pre.py
@@ -1,9 +1,7 @@
-import os
 import matplotlib.pyplot as plt
 import RayleighTaylor as rt
 from common import plot_terminators
 import settings as s
-from utils import save_but_not_show, fname_to_save
 
 
 
@@ -31,31 +29,7 @@ def plot_vertical_drift_and_pre(ds):
     return fig
 
 
-def save():
-    
-    save_in = "D:\\plots\\parameters\\drift\\"
-        
-    infile = "database/RayleighTaylor/reduced/300.txt"
-    df = rt.load_process(infile, apex = 300)
-    
-    for ds in rt.split_by_freq(df, freq_per_split = "10D"):
-        
-        name_to_save = fname_to_save(ds)
-        
-        dn = ds.index[0]
-        print("saving...", name_to_save)
-        
-        fig = plot_vertical_drift_and_pre(ds)
-        
-        month_name = dn.strftime("%m")
-                
-        save_it = os.path.join(
-            save_in, month_name, name_to_save 
-            )
-        save_but_not_show(fig, save_it)
-        
-save()
-        
+
 
 def main():
     infile = "database/RayleighTaylor/reduced/300.txt"
