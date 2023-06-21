449a7d27ace82c719112d4642792cea4c9c6edf4
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-06-21 06:20:54 -0300

    update init

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 from .drift_utils import load_drift
-from .drift import load_export, process_day
-from .process import fixed_frequencies
+from .drift import load_export
+from .SAO_process import fixed_frequencies
 from .utils import ionosonde_fname
 from .profilogram import load_profilogram
 from .PRE import drift, get_pre, add_vzp
