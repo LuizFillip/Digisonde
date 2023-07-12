28c7df3fb9c1d92708df244119f8557c8c7c416a
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-07-12 14:17:54 -0300

    new layout for plot

--- src/plotting/plot_vertical_drift_and_pre.py
+++ src/plotting/plot_vertical_drift_and_pre.py
@@ -3,34 +3,65 @@ from common import sun_terminator, load, sel_dates
 import settings as s
 import datetime as dt
 import digisonde as dg
+import numpy as np
+import pandas as pd
 
-def plot_vertical_drift_and_pre(ds):
+def plot_vertical_drift_and_pre(ds, dn):
     
     fig, ax = plt.subplots(
         figsize = (10, 5),
         dpi = 300)
     
-    ax.plot(ds["vz"], label = "Vz")
+    ax.plot(ds["vz"])
     
     ax.axhline(0, linestyle = "--")
-    ax.set(xlim = [ds.index[0], ds.index[-1]])
-    s.format_time_axes(ax)
+    
+    
     ax.set(ylabel = "Deriva vertical (m/s)", 
            xlim = [ds.index[0], ds.index[-1]], 
            ylim = [-75, 75])
     
-    ax.legend(loc = "upper left")
+    t, p = ds['vz'].idxmax(), ds['vz'].max()
     
+    ax.scatter(t, p)
+    
+    colors = ['b', 'r', 'k']
+    labels = [0, 125, 300]
+    for i, angle in enumerate([0, 12, 18]):
+        ax.axvline(sun_terminator(
+            dn, 
+            twilight_angle = angle), 
+            color = colors[i],
+            label = f'{labels[i]} km'
+            )
+        
+    ax.legend(loc = "upper left")
+    s.format_time_axes(ax)
     return fig
 
+def save_plots():
+    from utils import dn2FigureName, save_img
 
-dn = dt.datetime(2014, 1, 3, 18)
-delta = dt.timedelta(hours = 10)
-
-infile = 'database/Digisonde/process/SL_2014-2015/mean_hf.txt'
-
-df = dg.get_drift(load(infile))
-
-df = sel_dates(df, start = dn, end = dn + delta)
-
-plot_vertical_drift_and_pre(df)
+    infile = 'database/Digisonde/process/SL_2014-2015/mean_hf.txt'
+    
+    df = dg.get_drift(load(infile))
+    
+    
+    save_in = 'D:\\plots2\\PRE\\'
+    
+    for dn in np.unique(df.index.date):
+        
+        dn = pd.to_datetime(dn)
+        start = dn + dt.timedelta(hours = 18)
+        ds = sel_dates(
+            df, 
+            start = start, 
+            end = start + dt.timedelta(hours = 8)
+            )
+        
+        try:
+            f = plot_vertical_drift_and_pre(ds, dn)
+            
+            save_img(f, save_in + dn2FigureName(dn))
+        except:
+            continue
