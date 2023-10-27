70d3611f06367ece6086dac93547f401acb06f95
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-10-27 17:10:19 -0300

    updates

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
 from .drift_utils import load_drift
-from .utils import ionosonde_fname
+from .utils import ionosonde_fname, crop_image
 from .profilogram import load_profilogram
 from .PRE import get_pre, sel_between_terminators
 from .pre_parser import concat_all_pre_values
--- src/utils.py
+++ src/utils.py
@@ -2,34 +2,34 @@ import datetime as dt
 
 
 embrace_infos = {
-                "FZA0M": {"name": "Fortaleza", 
-                          "lat":  -3.73, 
-                          "lon": -38.522},
-                
-                "SAA0K": {"name": "Sao luis", 
-                          "lat":  -2.53,
-                          "lon": -44.296}, 
-                
-                "BLJ03": {"name": "Belem", 
-                          "lat": -1.4563, 
-                          "lon": -48.5013}, 
-                
-                "BVJ03": {"name": "Boa Vista", 
-                          "lat":  2.8701, 
-                          "lon": -60.7109},
-                
-                "CGK21": {"name": "Campo Grande", 
-                          "lat":  -20.4649, 
-                          "lon": -54.6218},
-                
-                "CAJ2M": {"name": "Cachoeira Paulista", 
-                          "lat": -22.7038, 
-                          "lon": -45.0093},
-                
-                "SMK29": {"name": "Santa Maria", 
-                          "lat": -29.6897, 
-                          "lon": -53.8043},
-                }
+    "FZA0M": {"name": "Fortaleza", 
+              "lat":  -3.73, 
+              "lon": -38.522},
+    
+    "SAA0K": {"name": "Sao luis", 
+              "lat":  -2.53,
+              "lon": -44.296}, 
+    
+    "BLJ03": {"name": "Belem", 
+              "lat": -1.4563, 
+              "lon": -48.5013}, 
+    
+    "BVJ03": {"name": "Boa Vista", 
+              "lat":  2.8701, 
+              "lon": -60.7109},
+    
+    "CGK21": {"name": "Campo Grande", 
+              "lat":  -20.4649, 
+              "lon": -54.6218},
+    
+    "CAJ2M": {"name": "Cachoeira Paulista", 
+              "lat": -22.7038, 
+              "lon": -45.0093},
+    
+    "SMK29": {"name": "Santa Maria", 
+              "lat": -29.6897, 
+              "lon": -53.8043},
+    }
         
 def get_infos(filename):
     
@@ -58,3 +58,12 @@ def ionosonde_fname(filename):
         
         
 
+def crop_image(
+        img, 
+        y = 50, 
+        x = 130, 
+        h = 900, 
+        w = 750
+        ):
+    
+    return img[y: y + h, x: x + w]
