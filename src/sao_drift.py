57ac500b06c577dee42eba030a3c5ac8aedd40b6
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2024-06-07 11:57:14 +0900

    deleting velocity function

--- src/sao_drift.py
+++ src/sao_drift.py
@@ -2,26 +2,9 @@ import digisonde as dg
 import numpy as np
 import pandas as pd
 import base as b
-import datetime as dt 
-
-
-
-PATH_FREQ = 'database/jic/freq/'
-
-
-def velocity(ds, col, smooth = True):
-    
-    if smooth:
-        ds[col] = b.smooth2(ds[col], 5)
-
-    ds[col] = (ds[col].diff() / ds["time"].diff()) / 3.6
-    
-    return ds.interpolate()
-    
 
 def vertical_drift(
         ds: pd.DataFrame, 
-        set_cols = None,
         smooth = None
         ) -> pd.DataFrame:
     
@@ -40,23 +23,23 @@ def vertical_drift(
         if col != "time":                         
 
             ds[col] = (ds[col].diff() / ds["time"].diff()) / 3.6
+            
+            if smooth is not None:
+                
+                ds[col] = b.smooth2(ds[col], smooth)
     
     ds["vz"] = np.mean(ds[cols], axis = 1)
-    # print(cols[:-1])
-    
-    if smooth is not None:
-        for col in cols:
-            ds[col] = b.smooth2(ds[col], smooth)
-    
+
     ds = ds.replace(0, float('nan'))
     return ds
 
 
-# def test():
-file = 'SAA0K_20151202(336).TXT'
-cols = list(range(5, 8, 1))
-ds = dg.IonoChar(file, cols).heights 
-
-df = vertical_drift(ds, smooth=None)
-
-df
+def test():
+    file = 'SAA0K_20151220(354).TXT'
+    # digisonde/data/chars/freqs/
+    cols = list(range(5, 8, 1))
+    ds = dg.IonoChar(file, cols).heights 
+    
+    df = vertical_drift(ds, smooth= 3)
+    
+    df['vz'].plot()
