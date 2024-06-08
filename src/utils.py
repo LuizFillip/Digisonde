97432183260eb2aa88664210cc000f8db37bb757
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2024-06-08 13:28:12 +0900

    rewrite variables

--- src/utils.py
+++ src/utils.py
@@ -8,23 +8,16 @@ def fn2dn(filename):
     args = filename.split("_")
     
     try:
-
-        str_datetime = args[1][:8] + args[1][13:-4]
-    
-        return dt.datetime.strptime(
-            str_datetime, "%Y%m%d%H%M%S"
-            )
+        date_str = args[1][:8] + args[1][13:-4]
+        fmt =  "%Y%m%d%H%M%S"
+        return dt.datetime.strptime(date_str, fmt)
     
     except:
-        str_datetime = args[1].split('.')[0]
-
-        return dt.datetime.strptime(
-            str_datetime, "%Y%m%d(%j)%H%M%S"
-            )
+        date_str = args[1].split('.')[0]
+        fmt = "%Y%m%d(%j)%H%M%S"
+        return dt.datetime.strptime(date_str, fmt)
 
         
-        
-
 def closest_iono(target, path_in):
     iono_times = [
         fn2dn(f) for f in
@@ -33,9 +26,10 @@ def closest_iono(target, path_in):
     return b.closest_datetime(iono_times, target)
 
 
-def path_ionogram(dn, target = None, site = 'SAA0K'):
+def path_ionogram(dn, target = None, site = 'SAA0K', root = 'E:\\'):
     folder_ion = dn.strftime('%Y%m%d')
-    path = f'database/ionogram/{folder_ion}'
+    
+    path = os.path.join(root, 'ionogram', folder_ion)
     
     if target is None:
         return f'{path}{site[0]}/'
@@ -51,8 +45,7 @@ def path_ionogram(dn, target = None, site = 'SAA0K'):
             site = 'FZA0M'
             closest_dn = closest_iono(target, path_in )
         
-        filename = closest_dn.strftime(
-            f'{site}_%Y%m%d(%j)%H%M%S.PNG')
+        filename = closest_dn.strftime(f'{site}_%Y%m%d(%j)%H%M%S.PNG')
         
         return site, f'{path_in}{filename}'
     
