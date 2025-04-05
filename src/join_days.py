77d5493de29587457cabd79c9dba8b75f5451d75
LuizFillip <luizfillip@outlook.com>
2025-04-05 11:37:28 -0300

    replacing functions

--- src/join_days.py
+++ src/join_days.py
@@ -6,25 +6,9 @@ import datetime as dt
 b.config_labels()
 
 
-
-
-def load_drift(
-        site, 
-        dn, 
-        smooth = 3,
-        cols = list(range(3, 10, 1))
-        
-        ):
-    file = dg.dn2fn(dn, site)
     
-    df = dg.IonoChar(file, cols, sum_from = None)
-    
-    df = df.drift(smooth)['vz'].to_frame(site)
-    df = df.interpolate()
-    df[site] = b.smooth2(df[site], 3)
-    return df
-    
-
+def dn2fn(dn, site):
+    return dn.strftime(f'{site}_%Y%m%d(%j).TXT')
 
 def join_iono_days(
         site, 
@@ -41,7 +25,7 @@ def join_iono_days(
         
         delta = dt.timedelta(days = i)
         
-        file =  dg.dn2fn(dn + delta, site)
+        file =  dn2fn(dn + delta, site)
         
         df = dg.IonoChar(file, cols, sum_from = None)
         
@@ -73,15 +57,19 @@ def quiet_time_avg(
     
     for dn in dates:
         
-        file =  dg.dn2fn(dn, site)
+        file =  dn2fn(dn, site)
         
         df = dg.IonoChar(file, cols, sum_from = None)
-        
+                
         ds = df.drift(
             smooth = smooth
-            ).set_index('time')['vz'].dropna()
+            )
         
-        out.append(ds.sort_index().to_frame(dn.day))
+        ds = ds.loc[ds.index.date == dn.date()]
+                
+        sel_vz = ds.set_index('time')['vz'].dropna()
+        
+        out.append(sel_vz.sort_index().to_frame(dn.day))
         
     df = pd.concat(out, axis = 1).mean(axis = 1)
     
@@ -116,7 +104,6 @@ def renew_index_from_date(df, dn):
     
     return df.to_frame('quiet')
 
-start_date = dt.datetime(2015, 12, 19)
 
 def repeat_quiet_days(
         site, 
@@ -139,83 +126,9 @@ def repeat_quiet_days(
     return pd.concat(out)
 
 
-sites = ['SAA0K', 'BVJ03'] #'CAJ2M', 
-
-
-
-def concat_disturbed_quiet(site, dn):
-    
-    df = quiet_time_avg(site)
-    
-    df = renew_index_from_date(df, dn)
-    
-    ds = load_drift(site, dn)
-    
-    return pd.concat([df, ds], axis = 1)
-
-
-def get_infos(ds, dn, site):
-    
-    delta = dt.timedelta(hours = 12)
-    
-    conds = [(ds.index > dn), 
-             (ds.index < dn - delta)]
-    
-    names = ['Pôr do sol', 'Pré amanhecer']
-    
-    out = []
-    for i, cond in enumerate(conds):
-        
-        df = ds.loc[cond].max().to_frame(names[i]).round(2).T
-        
-        df.rename(columns = {site: 'distur'}, inplace = True)
-        
-        df.columns = pd.MultiIndex.from_product(
-            [[names[i]], df.columns])
-        df.index = [site]
-        out.append(df)
-        
-    return pd.concat(out, axis = 1)
-
-
-def infos_in_sites(dn, name):
-    out = []
-    for site in sites:
-    
-        ds = concat_disturbed_quiet(site, dn)
-    
-        df = get_infos(ds, dn, site)
-        
-        
-        out.append(df)
-        
-        
-    return pd.concat(out)
-
-
-def run_phases(dn):
-    names = ["Fase principal", 'Fase de recuperação']
-    
-    out = []
-    for i, name in enumerate(names):
-        delta = dt.timedelta(days = i)
-        out.append(infos_in_sites(dn + delta, name))
-        
-        
-    return pd.concat(out, axis =1)
-    
-# site = 'SAA0K'
-
-# site = 'CAJ2M'
-dn = dt.datetime(2015, 12, 21, 21)
-
-# ds = concat_disturbed_quiet(site, dn)
-# # get_infos(ds, dn)
-# # name = 'Fase principal'
-# # df = infos_in_sites(dn, name)
-
-# # ds.plot()
-
-# get_infos(ds, dn, site)
-
-# run_phases(dn)
+# site = 'FZA0M'
+# quiet_time_avg(
+#         site,
+#         cols = list(range(3, 10, 1)), 
+#         smooth = 3
+#         )
