6ae48407d27b1ad503b64a299ed8ae1c3da27ea7
LuizFillip <luizfillip@outlook.com>
2025-09-05 13:37:08 -0300

    new format for results

--- src/summary_sao.py
+++ src/summary_sao.py
@@ -5,78 +5,122 @@ import base as b
 import GEO as gg 
 
 
+def concat_quiet_and_disturb(site = 'SAA0K'):
+      
+    dn = dt.datetime(2015, 12, 19)
+    
+    ds = dg.join_iono_days(
+            site, 
+            dn,
+            parameter = 'hF',
+            number = 4,
+            cols = list(range(3, 8, 1))
+            )
+    
+    ds[site] = b.running(ds[site], 3)
+    
+    df = dg.repeat_quiet_days(site, dn)
+    ds['q_mean'] = ds.index.map(df['mean'])
+    ds['q_std'] = ds.index.map(df['std'])
+    return ds
+
+
+
+def test_join_drift():
+    
+    site = 'SAA0K'
+    
+    dn = dt.datetime(2015, 12, 19)
+    
+    df = dg.join_iono_days(
+            site, 
+            dn,
+            parameter = 'drift',
+            cols = [5, 6]
+            )
+    
+    print(df)
+
+
 
 sites = [ 'SAA0K', 'BVJ03', 'FZA0M', 'CAJ2M', 'CGK21']
 
 
-def run_in_dates(site):
 
-    out = []
+
+def concat_quiet_storm(site, p):
     
-    dates = pd.date_range(
-        '2015-12-19', 
-        freq = '1D', 
-        periods = 4
+    start = dt.datetime(2015, 12, 19)
+    
+    qt = dg.repeat_quiet_days(
+         site, 
+         start, 
+         parameter = p, 
+         )
+    
+    if p == 'drift':
+        wsm = 10
+    else:
+        wsm = 3
+        
+    qt = qt.apply(lambda s: b.smooth2(s, wsm))
+    
+        
+    df = dg.join_iono_days(
+            site, 
+            start, 
+            parameter = p,
+            cols = [5, 6, 7]
+            )
+    
+    df[p] = b.smooth2(df[p], 3)
+    
+    df = df.loc[~df.index.duplicated(keep='first')]
+    
+    ds = pd.concat([df[p], qt['mean']], axis = 1) 
+    
+    ds.rename(
+        columns = {p: 'storm', 'mean': 'quiet'},
+        inplace = True
         )
-    
-    for dn in dates:
-        
-        file =  dg.dn2fn(dn, site)
-    
-        ds = dg.IonoChar(
-            file,  
-            sum_from = None
-            ).chars
-        
-        dusk = gg.dusk_from_site(
-                dn, 
-                site[:3].lower(),
-                twilight_angle = 18
-                )
-        
-        delta = dt.timedelta(hours = 2)
-        
-        sel_index = ds.loc[
-            ((ds.index > dusk - delta) & 
-            (ds.index < dusk + delta))
-            ].copy()
-        
-        dummy = pd.DataFrame()
-        dummy[dn] = [sel_index['hmF2'].max()]
-        dummy.index = [site]
-        
-        out.append(dummy.T)
-        
-    return pd.concat(out).sort_index()
 
-out = []
-# for site in sites:
-#     out.append(run_in_dates(site))
-    
-# df = pd.concat(out, axis = 1)
+    return ds
 
-# df 
-for site in sites:
-    dn = dt.datetime(2015, 12, 21, 6)
-    file =  dg.dn2fn(dn, site)
+
+
+def summary_from_ref_time(ref_dn, site, p = 'hmF2'):
+    ds = concat_quiet_storm(site, p)
     
-    ds = dg.IonoChar(
-        file,  
-        sum_from = None
-        ).chars
-    
-    # ds['hmF2'].plot(figsize = (12, 6))
     delta = dt.timedelta(hours = 2)
     
     sel = ds.loc[
-        ((ds.index > dn - delta) & 
-        (ds.index < dn + delta))
+        ((ds.index > ref_dn - delta) & 
+        (ds.index <ref_dn + delta))
         ].copy()
     
-    data = {site: sel['hmF2'].max()}
-    # print()
-    out.append(pd.DataFrame(
-        data, index = [sel['hmF2'].idxmax()]))
+    date = sel['storm'].idxmax()
     
-pd.concat(out)
+    ds = sel.loc[date].to_frame().T  
+    ds['time'] = date 
+    ds.index = [site]
+    
+    return ds 
+
+ref_dn = dt.datetime(2015, 12, 20, 20)
+
+
+out = []
+for site in sites:
+    dusk = gg.dusk_from_site(
+            ref_dn, 
+            site[:3].lower(),
+            twilight_angle = 18
+            )
+
+
+    out.append(summary_from_ref_time(dusk, site))
+    
+df = pd.concat(out)
+
+df 
 
